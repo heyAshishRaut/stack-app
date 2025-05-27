d3615d303f2a2c6c228a72d59dd6a8ee4b76b7c9
Ashish Raut <theashish32@gmail.com>
2025-05-27 14:10:33 +0530

    Initial Commit - 27 May, 2025 14:10 -Small screen design change

--- FRONTEND_3/src/Components/SmallScreenPage.tsx
+++ FRONTEND_3/src/Components/SmallScreenPage.tsx
@@ -1,13 +1,12 @@
 import star from "../assets/star.svg"
-import smallscreen from "../assets/smallscreen.png";
 
 function SmallScreenPage(){
     return (
         <div style={{
-            background: 'linear-gradient(to right, #2F0743, #41295a)'
-        }} className="w-screen h-screen">
-            <div>
-                <div className="h-[100px] w-[100%] pt-[30px] pl-[20px]">
+            background: 'linear-gradient(to left, #fff, #076585)'
+        }} className="w-screen h-[100vh]">
+            <div className="h-[10%] w-[100%] ">
+                <div className="pt-[30px] pl-[20px]">
                     <div className="absolute font-cabinet text-4xl z-10 text-white">Stack</div>
                     <div>
                         <img src={star} alt="" className="h-[70px] relative left-[67px] top-[-5px]" />
@@ -16,10 +15,8 @@ function SmallScreenPage(){
             </div>
 
             <div
-                className="w-[100%] font-cabinet text-2xl flex flex-col gap-y-[20px] justify-center items-center text-justify px-[40px] text-white">
-                <img src={smallscreen} alt="" className="h-[300px]" />
-                <div>This Stack is currently available only on larger screens (13 inches or wider). </div>
-                <div>Please switch to a laptop or desktop for the <span className="text-white bg-black px-[5px]">best</span> experience.</div>
+                className="h-[90%] w-[100%] text-stone-800 font-cabinet text-2xl flex justify-center items-center text-justify px-[50px]">
+                <div>This application is currently available only on <span className="text-white bg-stone-800 px-[5px]">larger screens</span> (13 inches or wider). </div>
             </div>
         </div>
     );
